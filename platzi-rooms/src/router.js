b3473c70d87d86b0fa9e0de3f4ee578cffd7d7c3
Eden <gresseden@gmail.com>
2020-05-07 22:20:30 -0400

    finalizando el curso

--- platzi-rooms/src/router.js
+++ platzi-rooms/src/router.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+import store from "./store";
 import HomePage from "./views/HomePage.vue";
 import SearchPage from "./views/SearchPage.vue";
 import NotFoundPage from "./views/NotFoundPage.vue";
@@ -11,7 +12,7 @@ import HousesPages from "./views/user/HousesPage.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -32,12 +33,18 @@ export default new Router({
     {
       path: "/user/profile",
       name: "ProfilePage",
-      component: ProfilePage
+      component: ProfilePage,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/user/houses",
       name: "HousesPages",
-      component: HousesPages
+      component: HousesPages,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "/house",
@@ -46,7 +53,10 @@ export default new Router({
     {
       path: "/house/new",
       name: "CreateHousePage",
-      component: CreateHousePage
+      component: CreateHousePage,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: "*",
@@ -55,3 +65,19 @@ export default new Router({
     }
   ]
 });
+
+// Gards
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    if (store.state.authId) {
+      next();
+    } else {
+      next({ name: "HomePage" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
